7a93172051904eb149024cedb522b78997a79dfa
salma <salma@192.168.0.5>
2016-06-01 21:59:09 -0400

    Hasan vai pussed the code

---
+++ GitPractise/src/com/gitsonly/GitAutomation.java
@@ -0,0 +1,16 @@
+package com.gitsonly;
+
+import org.testng.annotations.Test;
+
+public class GitAutomation {
+	
+	@Test
+	public void TestOne(){
+		System.out.println("practicing GIT> testOne");
+	}
+	@Test
+	public void TestTwo(){
+		System.out.println("practicing GIT> testTwo");
+	}
+
+}
